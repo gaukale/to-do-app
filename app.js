147288581eeeb8c5bf65e9bfcd372697dad3c88f
Gauri <gauri@examle.com>
2019-03-02 20:27:52 -0800

    Dom-scripting-part1 assignment

--- app.js
+++ app.js
@@ -13,20 +13,28 @@ function onReady() {
    // create a new input
    let checkbox = document.createElement('input');
 
+   let removeLi = document.createElement("Button");
+
    // set the input's type to checkbox
    checkbox.type = "checkbox";
+
+   // set the inputs type to button//
+   removeLi.type = "button";
+
    // set the title
    newLi.textContent = title;
+   removeLi.textContent = "Delete";
 
    // attach the checkbox to the li
    newLi.appendChild(checkbox);
+   newLi.appendChild(removeLi);
+
    // attach the li to the ul
     toDoList.appendChild(newLi);
+
     //empty the input
     newToDoText.value = '';
-
   });
-
   }
 window.onload = function() {
       onReady();
