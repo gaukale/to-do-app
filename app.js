ea28e918524cc878212c285e60892b8c00691b2e
Gauri <gauri@examle.com>
2019-03-05 21:05:15 -0800

    first

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-   const newToDoText = document.getElementById('newToDoText');
-   const toDoList = document.getElementById('toDoList');
 
-   addToDoForm.addEventListener('submit', event => {
-   event.preventDefault();
-     // get the text
-    let title = newToDoText.value;
-    // create a new li
-   let newLi = document.createElement('li');
+  let id = 0;
+  const toDos = [];                                                    //state is array of to-dos//
+  const addToDoForm = document.getElementById('addToDoForm');               //access the HTML form//
 
-   // create a new input
-   let checkbox = document.createElement('input');
 
-   let removeLi = document.createElement("Button");
+  function createNewToDo() {                                         //to create new to-dos. we added this function//
 
-   // set the input's type to checkbox
-   checkbox.type = "checkbox";
-
-   // set the inputs type to button//
-   removeLi.type = "button";
-
-   // set the title
-   newLi.textContent = title;
-   removeLi.textContent = "Delete";
-
-   // attach the checkbox to the li
-   newLi.appendChild(checkbox);
-   newLi.appendChild(removeLi);
-
-   // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
-    newToDoText.value = '';
+  const newToDoText = document.getElementById('newToDoText');       //access the text input,to get the text for the title of each to-do//
+  if (!newToDoText.value) { return; }                              // to prevent adding empty to-dos by executing a return if newToDoText is falsy
+  toDos.push({                                                    //add the new to-do to the toDos array using the push() method//
+      title: newToDoText.value,                                   //assign the value of the text input, newToDoText to the title key.//
+      complete: false,                                            //key called complete and initialize it to false//
+      id: id++
   });
+
+  newToDoText.value = '';                                         //clear the text input for the user//
+  renderTheUI();                                                 //call renderTheUI() each time the state changes//
+}
+
+  function renderTheUI(){
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+                                                                       //before the  forEach() function, set newLi to an empty string//
+    toDos.forEach(function(toDo) {                                    //Using forEach method will render each to-do as a li in the ul//
+
+      const newLi = document.createElement('li');                     //create the li and checkbox//
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;                                 //add the toDo's title text to newLi//
+      let removeLi = document.createElement("Button");
+      removeLi.textContent = "Delete" ;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(removeLi);                                 //update the DOM//
+
+});
+
   }
+  addToDoForm.addEventListener('submit', event => {                 //dd the event listener, using the submit event of the form element//
+  event.preventDefault();
+  createNewToDo();                                                 //then call the function//
+  });
+
 window.onload = function() {
-      onReady();
- };
+        onReady();
+};
